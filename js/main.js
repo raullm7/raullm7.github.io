b3cb9c8a87b45a626fc0a91672695dcc8659f562
Raul Lozano Martin <raullm7@gmail.com>
2017-10-07 15:24:52 +0100

    Modified everything

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ window.onload = function() {
 
 window.onscroll = function() {
   el = document.getElementById('scroll-top');
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
       el.style.display = "block";
   } else {
       el.style.display = "none";
@@ -21,11 +21,24 @@ function topFunction() {
     top: 0,
     behavior: 'smooth'
   });
-}
+};
 
-function goToSection(number) {
-  $('html', 'body').animate({
-    scrollTop: 200,
-    behavior: 'smooth'
-  })
-}
+jQuery(document).ready(function(){
+	if( $('.cd-stretchy-nav').length > 0 ) {
+		var stretchyNavs = $('.cd-stretchy-nav');
+
+		stretchyNavs.each(function(){
+			var stretchyNav = $(this),
+				stretchyNavTrigger = stretchyNav.find('.cd-nav-trigger');
+
+			stretchyNavTrigger.on('click', function(event){
+				event.preventDefault();
+				stretchyNav.toggleClass('nav-is-visible');
+			});
+		});
+
+		$(document).on('click', function(event){
+			( !$(event.target).is('.cd-nav-trigger') && !$(event.target).is('.cd-nav-trigger span') ) && stretchyNavs.removeClass('nav-is-visible');
+		});
+	}
+});
