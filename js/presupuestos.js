e85dbaedf211dc33c5211ee6730767d28896a00b
Raul Lozano Martin <raullm7@gmail.com>
2019-06-14 17:14:26 +0200

    Fixed presupuestos

--- js/presupuestos.js
+++ js/presupuestos.js
@@ -11,6 +11,8 @@ function calcularPrecios() {
   const plastificadoDosCaras = document.getElementById('plastificadoDosCaras');
   const tintas = document.getElementById('tintas');
 
+  document.getElementById('precio').textContent = "";
+
   const url = 'https://presupuestosgraficasandalusi.com/presupuesto?tirada=' +
               tirada.value + '&paginas=' + paginas.value + '&dimension=' + dimension.value +
               '&papelInt=brillo&gramajeInt=' + gramajeInt.value + '&papelExt=brillo&gramajeExt=' + gramajeExt.value +
